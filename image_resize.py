411a2563b82a10f548d78c7025ba3896d284cd87
maltsev <arelav90@mail.ru>
2018-06-06 16:35:21 +0300

    add size

--- image_resize.py
+++ image_resize.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import argparse
 import os
+import glob
 
 
 def get_image(path_to_image):
@@ -17,24 +18,30 @@ def get_arguments():
         description='Image resize program')
     parser.add_argument('-i', '--input_file',
                         metavar='<path_to_file>',
-                        dest='path',
+                        dest='input_path',
                         required=True,
                         help='full path to the image which '
                              'you need to transform')
     parser.add_argument('--width',
                         metavar='N',
+                        dest='width',
                         required=False,
                         help='width of output image (pix)')
     parser.add_argument('--height',
                         metavar='N',
+                        dest='height',
                         required=False,
                         help='height of output image (pix)')
     parser.add_argument('--scale',
                         metavar='N',
+                        dest='scale',
                         required=False,
+                        default=1,
                         help='scale of output image (from 0)')
     parser.add_argument('-o', '--output_directory',
                         metavar='<output_directory>',
+                        dest='output_path',
+                        required=False,
                         help='path to output file directory')
     return parser.parse_args()
     # print(args.accumulate(args.integers))
@@ -42,6 +49,14 @@ def get_arguments():
 
 if __name__ == '__main__':
     args = get_arguments()
-    get_image(args.path)
-
-
+    if os.path.isfile(args.input_path):
+        img = get_image(args.input_path)
+        file_name, file_ext = os.path.splitext(args.input_path)
+        img_size = img.size
+        if args.width and args.height:
+            new_img_size = int(args.width), int(args.height)
+            new_img = img.resize(new_img_size)
+            new_img.save(file_name + '__' + args.width + 'x' +
+                         args.height + file_ext)
+    else:
+        print('File does not exist')
