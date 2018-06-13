83f4676896cdea8f9acabda6caee41dac0a1a3e7
maltsev <arelav90@mail.ru>
2018-06-13 11:09:17 +0300

    final first iterration

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # Image Resizer
 
-[TODO. There will be project description]
+Script can help you to resize image as you wish...
+
+# How to use
+
+To use script you need specify full path to image. You can use arguments to resize image with special params:
+```
+usage: image_resize.py [-h] -I <path_to_file> [-W N] [-H N] [-S N]
+                       [-O <output_directory>]
+
+Image resize program
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -I <path_to_file>, --input_file <path_to_file>
+                        full path to the image which you need to transform
+  -W N, --width N       width of output image (pix)
+  -H N, --height N      height of output image (pix)
+  -S N, --scale N       scale of output image (from 0)
+  -O <output_directory>, --output_directory <output_directory>
+                        path to output file directory
+```
+
+Example (Windows):
+```cmd
+C:\>python C:\12_image_resize\image_resize.py -I C:\pic.jpg -S 0.5
+```
+Result - saved image "pic__375x211.jpg" with ratio 0.5. 
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -9,6 +9,7 @@ def generate_parser():
     parser = argparse.ArgumentParser(
         description='Image resize program')
     parser.add_argument('-I', '--input_file',
+                        type=str,
                         metavar='<path_to_file>',
                         dest='input_path',
                         required=True,
@@ -27,12 +28,14 @@ def generate_parser():
                         required=False,
                         help='height of output image (pix)')
     parser.add_argument('-S', '--scale',
-                        type=int,
+                        type=float,
                         metavar='N',
                         dest='scale',
                         required=False,
                         help='scale of output image (from 0)')
     parser.add_argument('-O', '--output_directory',
+                        type=str,
+                        default='',
                         metavar='<output_directory>',
                         dest='output_directory',
                         required=False,
@@ -40,17 +43,17 @@ def generate_parser():
     return parser
 
 
-def check_parser_arguments(input_path, width, height, scale, output_directory, parser):
+def check_parser_arguments(input_path, width, height, scale,
+                           output_directory, parser):
     if input_path and not os.path.isfile(input_path):
         parser.error('Input path is not a file')
     if scale and int(scale) < 0:
         parser.error('Scale must be positive')
     if scale and (width or height):
-        parser.error('You should not specify width or height with scale together')
+        parser.error('You should not specify width or height with '
+                     'scale together')
     if output_directory and not(os.path.isdir(output_directory)):
         parser.error('Wrong path to output directory')
-    # if args.width and args.height:
-    #     return 'WARN: The proportions may not coincide with the original image'
 
 
 def get_image(path_to_image):
@@ -60,51 +63,45 @@ def get_image(path_to_image):
 def get_new_size(image, args):
     image_width, image_height = image.size
     if args.width and not(args.height) and not (args.scale):
-        new_width = args.width
-        new_height = image_height * args.width / image_width
+        new_width = int(args.width)
+        new_height = int(image_height * args.width / image_width)
         return new_width, new_height
     elif args.height and not(args.width) and not(args.scale):
-        new_height = args.height
-        new_width = image_width * args.height / image_height
+        new_height = int(args.height)
+        new_width = int(image_width * args.height / image_height)
         return new_width, new_height
     elif args.scale and not(args.width) and not(args.height):
-        new_width = image_width * args.scale
-        new_height = image_height * args.scale
+        new_width = int(image_width * args.scale)
+        new_height = int(image_height * args.scale)
         return new_width, new_height
     elif args.width and args.height and not(args.scale):
-        new_width = args.width
-        new_height = args.height
+        new_width = int(args.width)
+        new_height = int(args.height)
+        print('warn: The proportions may not coincide with the original image')
         return new_width, new_height
     else:
         return image_width, image_height
 
 
-def resize_image(args):
-    file_name, file_ext = os.path.splitext(args.input_path)
-    new_img_size = int(args.width), int(args.height)
-    new_img = img.resize(new_img_size)
-    # new_img.save(path_to_result + file_name + '__' + args.width + 'x' +args.height + file_ext)
+def resize_image(image, new_sizes):
+    return image.resize(new_sizes)
+
+
+def save_resized_image(image_path, resized_image, resized_image_folder,
+                       width, height):
+    file_name, file_ext = os.path.splitext(image_path)
+    resized_image.save(resized_image_folder + file_name + '__' +
+                       str(width) + 'x' + str(height) + file_ext)
 
 
 if __name__ == '__main__':
     parser = generate_parser()
     args = parser.parse_args()
-    check_parser_arguments(args.input_path, args.width, args.height, args.scale, args.output_directory, parser)
+    check_parser_arguments(args.input_path, args.width, args.height,
+                           args.scale, args.output_directory, parser)
     img = get_image(args.input_path)
     new_sizes = get_new_size(img, args)
-    print(new_sizes)
-
-    # image = get_image(args.input_path)
-    # image_width, image_height = image.size
-    # new_size = get_new_size(image_width, image_height, args)
-    # print(new_size)
-
-    # if args.output_directory:
-    #     path_to_result = args.output_directory
-    # else:
-    #     path_to_result = ''
-    # if os.path.isfile(args.input_path):
-    #     path_to_original = args.input_path
-    #     resize_image(path_to_original, path_to_result)
-    # else:
-    #     print('File does not exist')
+    new_width, new_height = new_sizes
+    new_img = resize_image(img, new_sizes)
+    save_resized_image(args.input_path, new_img, args.output_directory,
+                       new_width, new_height)
